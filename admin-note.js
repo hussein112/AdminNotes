e1b554c5d89db0d564d2e196e7e43f83dc239e45
hussein112 <husseinkhalil420@gmail.com>
2023-02-20 00:49:21 +0200

    Load Bootstrap Icons if not Loaded

--- admin-note.js
+++ admin-note.js
@@ -2,4 +2,18 @@ import { NoteController } from './noteController.js';
 
 window.onload = function(){
     NoteController.index();
+    loadCss();
+}
+
+function loadCss () {
+    var ss = document.styleSheets;
+    for (var i = 0, max = ss.length; i < max; i++) {
+        if (ss[i].includes("https://cdn.jsdelivr.net/npm/bootstrap-icons"))
+            return;
+    }
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css";
+
+    document.getElementsByTagName("head")[0].appendChild(link);
 }
